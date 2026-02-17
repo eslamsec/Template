652e52a07e1910c2635ebf991ee96c24d843f146
Eslam <eslambadawy25688@gmail.com>
2026-02-17 15:13:23 +0200

    menu page

---
+++ menu.blade.php
@@ -0,0 +1,17 @@
+@extends('theme.main')
+
+@section('content')
+    
+ <div class="card-body row " id="sidebar-menu">
+     <div class="col-sm-3 ">
+         <p class="plabel">Accounts</p> 
+         <ul class="metismenu report_menu" id="side-menu"> 
+             <li>
+                 <a href="" class="nav_link">
+                     Group<span class="menu-arrow"></span>
+                 </a>
+             </li> 
+         </ul> 
+     </div> 
+ </div>
+@endsection 
